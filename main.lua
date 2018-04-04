c6cda5b73b83653b6c02cd10bde63926796fca0a
Andrew Minnich <aminnich3@gmail.com>
2018-04-04 09:14:14 -0400

    reorganize

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local cartographer = require 'cartographer'
 
-local testMap = cartographer.load 'test.lua'
+local testMap = cartographer.load 'demo/test.lua'
 
 function love.draw()
 	testMap.layers.Back:draw()
