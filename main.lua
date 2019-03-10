ecb4f2129f9875a213b96cdd2461a3584207076b
Andrew Minnich <aminnich3@gmail.com>
2019-03-10 14:14:07 -0400

    remove unnecessary test maps

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local cartographer = require 'cartographer'
 
-local testMap = cartographer.load 'demo/groups.lua'
+local testMap = cartographer.load 'demo/test.lua'
 
 function love.update(dt)
 	testMap:update(dt)
