8380dc628f031a9874689c8dd4bb5b9a4670278b
Andrew Minnich <aminnich3@gmail.com>
2019-03-10 14:26:02 -0400

    object drawing test map

---
+++ demos/platformer/objects.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="objects" tilewidth="16" tileheight="24" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="24" source="door.png"/>
+ </tile>
+ <tile id="1">
+  <image width="14" height="8" source="key.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="20" source="player.png"/>
+ </tile>
+</tileset>
