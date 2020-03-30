f5d9f305f26cbf6c159d761b35d63f64ba8ec508
Yozzaxia1311 <yozzaxia@gmail.com>
2020-03-29 23:48:54 -0400

    Respect formatting again

--- cartographer.lua
+++ cartographer.lua
@@ -50,11 +50,11 @@ end
 -- Decompress tile layer data
 -- https://github.com/karai17/Simple-Tiled-Implementation/blob/master/sti/utils.lua#L67
 function getDecompressedData(data)
-	local ffi = require("ffi")
+	local ffi = require 'ffi'
 	local d = {}
-	local decoded = ffi.cast("uint32_t*", data)
+	local decoded = ffi.cast('uint32_t*', data)
 
-	for i = 0, (data:len() / ffi.sizeof("uint32_t")) - 1 do
+	for i = 0, (data:len() / ffi.sizeof('uint32_t')) - 1 do
 		table.insert(d, tonumber(decoded[i]))
 	end
 
