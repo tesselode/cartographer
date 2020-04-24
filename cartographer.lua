da316f431a53fc28d15578435d47fc3f63b43195
Andrew Minnich <aminnich3@gmail.com>
2020-04-24 01:32:13 -0400

    version/copyright bump

--- cartographer.lua
+++ cartographer.lua
@@ -1,12 +1,12 @@
 --- Tiled map loading and drawing library for LÖVE.
 local cartographer = {
-	_VERSION = 'Cartographer v2.1',
+	_VERSION = 'Cartographer v2.2',
 	_DESCRIPTION = 'Simple Tiled map loading for LÖVE.',
 	_URL = 'https://github.com/tesselode/cartographer',
 	_LICENSE = [[
 		MIT License
 
-		Copyright (c) 2019 Andrew Minnich
+		Copyright (c) 2020 Andrew Minnich
 
 		Permission is hereby granted, free of charge, to any person obtaining a copy
 		of this software and associated documentation files (the "Software"), to deal
